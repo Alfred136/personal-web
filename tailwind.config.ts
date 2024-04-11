72a5d9dddc3ba5475e8ed42d920149d192373606
Alfred Tse <alfredtse136@gmail.com>
2024-04-11 17:46:08 -0400

    Add styling config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      fontFamily: {
+        pixel: 'Pixelify Sans, sans-serif'
+      },
+      colors: {
+        midnight: '#272744',
+        night: '#494d7e',
+        evening: '#8b6d9c',
+        sunset: '#c69fa5',
+        afternoon: '#f2d3ab',
+        morning: '#fbf5ef'
+      },
+      screens: {
+        xs: '480px',
+        ss: '620px',
+        sm: '768px',
+        md: '1060px',
+        lg: '1200px',
+        xl: '1440px',
+        xxl: '1700px'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
